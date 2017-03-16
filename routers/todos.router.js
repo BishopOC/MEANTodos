dc49c0d800b06f4227f49b0706834d5f91cdd389
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-03-16 19:22:33 -0400

    angular

---
+++ routers/todos.router.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var todosRouter = express.Router();
+var Todos = require('../models/todos.models');
+
+todosRouter.use(express.static(__dirname + '/../public'));
+todosRouter.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname + '/../public/html'});
+});
+
+todosRouter.get('/todos', function(req, res){
+  Todos.find({}, function(err, docs){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        todos: docs
+      });
+    }
+  });
+});
+
+todosRouter.get('/todos/:id', function(req, res){
+  Todos.find({_id: req.params.id}, function(err, docs){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(200).json({
+        todos: docs
+      });
+    }
+  });
+});
+
+todosRouter.post('/todos', function(req, res){
+  var todos = new Todos(req.body);
+  todos.save(function(err, docs){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(201).json({
+        msg: 'Success'
+      });
+    }
+  });
+});
+
+todosRouter.put('/todos/:id', function(req, res){
+  Todos.findOneAndUpdate({_id: req.params.id}, req.body, function(err, docs){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully Updated'
+      });
+    }
+  });
+});
+
+todosRouter.delete('/todos/:id', function(req, res){
+  Todos.remove({_id: req.params.id}, function(err, docs){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully delete'
+      });
+    }
+  });
+});
+
+module.exports = todosRouter;
